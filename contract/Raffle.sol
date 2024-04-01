75532165326d99d595abc7a8ff79502a93ad1f9a
kaki <chenjiaqi@mabangerp.com>
2024-04-01 10:04:18 +0800

    format

--- contract/Raffle.sol
+++ contract/Raffle.sol
@@ -6,38 +6,38 @@ import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     event RaffleEnter(address indexed player);
-    constructor(address vrfCoordinatorV2 ,uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
     function enterRaffle() public payable {
-        if(msg.value<i_entranceFee){ 
+        if (msg.value < i_entranceFee) {
             revert Raffle_NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
-    } 
-
-    function pickRandomWinner() external{
-        
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+    function pickRandomWinner() external {}
 
-    }
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
-
-
-    function getEntranceFee() public view returns(uint256){
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns(address){
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
